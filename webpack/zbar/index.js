a4c705a3b5254774dfbdc2de0ac380fef1b1f322
Joey Fang <josudoey@gmail.com>
2021-10-25 21:22:23 +0800

    ci: add lose file

---
+++ webpack/zbar/index.js
@@ -0,0 +1,18 @@
+const path = require('path')
+module.exports = {
+  entry: path.resolve(__dirname, './entry.js'),
+  output: {
+    path: path.resolve(__dirname, '../../dist'),
+    filename: 'zbar.js',
+    libraryTarget: 'commonjs2'
+  },
+  mode: 'production',
+  devtool: 'source-map',
+  resolve: {
+    fallback: {
+      fs: false,
+      path: false,
+      crypto: false
+    }
+  }
+}
